2efeb0eb3b47ff54908cdff67b50bf39e19743d9
libingbing <bike_travel@163.com>
2016-08-27 12:11:47 +0800

    land added

--- src/offb_simulation_test.cpp
+++ src/offb_simulation_test.cpp
@@ -1,90 +1,112 @@
-/**
+ /**
 * @file     : offb_simulation_test.cpp
-* @brief    : offboard simulation test: demon rewritten -> 4 setpoints flight -> complete state machine.
+* @brief    : offboard simulation test: demo rewritten -> 4 setpoints flight -> complete state machine.
 * @author   : libn
 * @Time     : Aug 25, 201610:06:42 PM
 */
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-#include "state_machine/CommandBool.h"
-#include "state_machine/SetMode.h"
-#include "state_machine/State.h"
+#include <state_machine/CommandBool.h>
+#include <state_machine/SetMode.h>
+#include <state_machine/State.h>
+#include <state_machine/CommandTOL.h>
 
 state_machine::State current_state;
-void state_cb(const state_machine::State::ConstPtr& msg)
-{
-	current_state = *msg;
+void state_cb(const state_machine::State::ConstPtr& msg){
+    current_state = *msg;
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc,argv,"offb_node_test");
-	ros::NodeHandle nh;
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
 
-	ros::Subscriber state_sub = nh.subscribe<state_machine::State>("mavros/state",10,state_cb);
-	ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",10);
-	ros::ServiceClient arming_client = nh.serviceClient<state_machine::CommandBool>("mavros/cmd/arming");
-	ros::ServiceClient set_mode_client = nh.serviceClient<state_machine::SetMode>("mavros/set_mode");
+    ros::Subscriber state_sub = nh.subscribe<state_machine::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<state_machine::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<state_machine::SetMode>
+            ("mavros/set_mode");
 
-	ros::Rate rate(20.0);
+    // takeoff and land service
+    // ros::ServiceClient takeoff_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/takeoff");
+    ros::ServiceClient land_client = nh.serviceClient<state_machine::CommandTOL>("mavros/cmd/land");
+    state_machine::CommandTOL landing_cmd;
+    landing_cmd.request.min_pitch = 1.0;
 
-	while(ros::ok() && current_state.connected)
-	{
-		ros::spinOnce();
-		rate.sleep();
-	}
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
 
-	geometry_msgs::PoseStamped pose;
-	pose.pose.position.x =0;
-	pose.pose.position.y =0;
-	pose.pose.position.z =2;
+    // wait for FCU connection
+    while(ros::ok() && current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
 
-	for(int i =100; ros::ok() && i>0; i--)
-	{
-		local_pos_pub.publish(pose);
-		ros::spinOnce();
-		rate.sleep();
-	}
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 2;
 
-	state_machine::SetMode offb_set_mode;
-	offb_set_mode.request.custom_mode = "OFFBOARD";
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
 
-	state_machine::CommandBool arm_cmd;
-	arm_cmd.request.value = true;
+    state_machine::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
 
-	ros::Time last_request = ros::Time::now();
+    state_machine::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
 
-	while(ros::ok())
-	{
-		if(current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request) > ros::Duration(5.0))
+    ros::Time last_request = ros::Time::now();
+    ros::Time t = ros::Time::now();
+
+    while(ros::ok()){
+        if( current_state.mode != "OFFBOARD" && current_state.mode != "AUTO.LAND" &&	/* set offboard mode for the first time. -libn */
+          (ros::Time::now() - last_request > ros::Duration(5.0)))
+        {
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.success){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed && current_state.mode != "AUTO.LAND" &&	/* set armed for the first time. -libn */
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                    t = ros::Time::now();
+                }
+                last_request = ros::Time::now();
+            }
+
+        }
+
+		// landing
+		if(current_state.armed && ros::Time::now() - t > ros::Duration(10.0))	/* set landing mode until uav stopped. -libn */
 		{
-			if(set_mode_client.call(offb_set_mode) && offb_set_mode.response.success)
+			if(current_state.mode != "AUTO.LAND" && (ros::Time::now() - last_request > ros::Duration(5.0)))
 			{
-				ROS_INFO("OFFBOARD enabled");
-			}
-			last_request = ros::Time::now();
-		}
-		else
-		{
-			if(!current_state.armed && (ros::Time::now())-last_request > ros::Duration(5.0))
-			{
-				if(arming_client.call(arm_cmd) && arm_cmd.response.success)
+				if(land_client.call(landing_cmd) && landing_cmd.response.success)
 				{
-					ROS_INFO("Vehicle armed");
+					ROS_INFO("AUTO LANDING!");
 				}
 				last_request = ros::Time::now();
 			}
 		}
-		local_pos_pub.publish(pose);
-		rate.sleep();
 
 
-	}
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
 
-	return 0;
-
-
-
-
+    return 0;
 }
