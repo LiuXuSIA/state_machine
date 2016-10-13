98adc3110bba57d34c49afc2273b1d9a5c879356
bing0037 <bike_travel@163.com>
2016-10-13 12:32:44 +0800

    circle_distance calculation bug fixed

--- src/offb_simulation_test.cpp
+++ src/offb_simulation_test.cpp
@@ -129,7 +129,7 @@ geometry_msgs::TwistStamped vel_pub;	/* velocity setpoint to be published. -libn
 
 
 /* calculate distance */
-double circle_distance(double x1, double y1, double z1, double x2, double y2, double z2)
+double circle_distance(double x1, double x2, double y1, double y2, double z1, double z2)
 {
     return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
 }
