d50c4be9d89bf884d12db1be6b6d6594c4e28189
LiuXuSIA <liuxu.ccc@gmail.com>
2018-09-26 16:30:17 +0800

    vision_position

--- src/vision_position.cpp
+++ src/vision_position.cpp
@@ -10,34 +10,27 @@
 #include <serial/serial.h> 
 #include <std_msgs/String.h> 
 #include <std_msgs/Empty.h> 
-#include <std_msgs/Float32.h> 
-#include <state_machine/Distance.h> 
-#include <vector> 
+#include <std_msgs/Float32.h>
+#include <state_machine/Vision_Position_Raw.h>
 
-using namespace std;
-
-//distance sensor
-const int write_data[4] = {0x80, 0x06, 0x02, 0x78}; //rad once
-const int write_data1[4] = {0x80, 0x06, 0x03, 0x77};//read continuous
-
-const vector<uint8_t> read_once(write_data,write_data+4);
-const vector<uint8_t> read_continuous(write_data1,write_data1+4);
 
 serial::Serial ser; 
 
+state_machine::Vision_Position_Raw vision_position_raw;
+
 int main (int argc, char** argv) 
 { 
-    ros::init(argc, argv, "distance_sensor_node"); 
+    ros::init(argc, argv, "vision_position_node"); 
 
-    ros::NodeHandle nh; 
- 
-    ros::Publisher distance_pub = nh.advertise<state_machine::Distance>("distance", 10); 
+    ros::NodeHandle nh;  
+
+    ros::Publisher vision_position_pub = nh.advertise<state_machine::Vision_Position_Raw>("vision_position", 10); 
 
     ros::Rate rate(10.0); 
 
     try 
     { 
-        ser.setPort("/dev/ttyUSB0"); 
+        ser.setPort("/dev/ttyUSB1"); 
         ser.setBaudrate(9600); 
         serial::Timeout to = serial::Timeout::simpleTimeout(1000); 
         ser.setTimeout(to); 
@@ -57,22 +50,36 @@ int main (int argc, char** argv)
     { 
         return -1; 
     } 
-    
-    ser.write(read_continuous);
 
     while(ros::ok()) 
     { 
-        if(ser.available()){ 
+        if(ser.available())
+        { 
             ROS_INFO_STREAM("Reading:"); 
-            std_msgs::String result; 
-            state_machine::Distance distance;
-            vector<uint8_t> data_dis;
-            ser.read(data_dis,ser.available()); 
+            std_msgs::String position_data; 
+            float position_x,position_y,position_z,position_d;
+            position_data.data = ser.read(ser.available()); 
+            ROS_INFO_STREAM("Read: " << position_data.data);
 
-            distance.distance = (data_dis[3]-48)*100+(data_dis[4]-48)*10+(data_dis[5]-48)+
-                       (data_dis[7]-48)*0.1+(data_dis[8]-48)*0.01+(data_dis[9]-48)*0.001;
-            ROS_INFO("distance:%f",distance.distance);
-            distance_pub.publish(distance); 
+            if(position_data.data.substr(0,4) == "SSSS" &&
+                position_data.data.substr(27,4) == "EEEE")
+            {
+                position_x = atof(position_data.data.substr(5,5).c_str());
+                position_y = atof(position_data.data.substr(11,5).c_str());
+                position_z = atof(position_data.data.substr(17,5).c_str());
+                position_d = atof(position_data.data.substr(23,4).c_str());
+
+                ROS_INFO("x:%f",position_x);
+                ROS_INFO("y:%f",position_y);
+                ROS_INFO("z:%f",position_z);
+                ROS_INFO("d:%f",position_d);
+
+                vision_position_raw.x = position_x/1000;
+                vision_position_raw.y = position_y/1000;
+                vision_position_raw.z = position_z/1000;
+
+                vision_position_pub.publish(vision_position_raw);
+            }
         } 
 
         ros::spinOnce(); 
