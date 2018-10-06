bb83e1cddc1f0db3d560f4c90bfa8618f49b8403
LiuXuSIA <liuxu.ccc@gmail.com>
2018-10-06 10:13:37 +0800

    grab test

---
+++ src/grab_test.cpp
@@ -0,0 +1,92 @@
+/*************************************************************************
+@file           grab_test.cpp
+@date           2018/10/02 
+@author         liuxu
+@email          liuxu.ccc@gmail.com
+@description    grab_test  grab->10s->release
+*************************************************************************/
+
+#include <ros/ros.h>
+#include <state_machine/State.h>
+
+/***************************function declare****************************/
+void state_machine_fun(void);
+
+/***************************variable definition*************************/
+
+//state_machine state
+static const int initial = 1;
+static const int grab = 7;
+static const int release = 13;
+
+//mission 
+int current_pos_state = initial;
+
+//time
+ros::Time last_time;
+
+state_machine::State current_state;
+void state_cb(const state_machine::State::ConstPtr& msg)
+{
+    current_state = *msg;
+}
+
+/*****************************main function*****************************/
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offboard_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<state_machine::State>("mavros/state",10,state_cb);
+
+    ros::Rate rate(10.0);
+
+    while(ros::ok() && !current_state.connected)
+    {
+    	ros::spinOnce();
+    	rate.sleep();
+    }
+
+    ROS_INFO("Connect successfully!!");
+ 
+    last_time = ros::Time::now();
+
+	while(ros::ok())
+	{
+        state_machine_fun();
+
+		ros::spinOnce();
+		rate.sleep();
+	}
+
+	return 0;
+}
+
+/***********************************state_machine function***********************************/
+void state_machine_fun(void)
+{
+    switch(current_pos_state)
+    {
+        case initial:
+        {
+            if(ros::Time::now() - last_time > ros::Duration(10.0))
+            {
+                current_pos_state = grab;
+                last_time = ros::Time::now();
+            }
+        }
+        break;
+        case grab:
+        {
+            if(ros::Time::now() - last_time > ros::Duration(5.0))
+            {
+                current_pos_state = release;
+                last_time = ros::Time::now();
+            }
+        }
+        break;
+        case release:
+        break;
+    }
+            
+}
